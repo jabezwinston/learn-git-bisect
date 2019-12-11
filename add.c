0576fbc6ab2ff5efbc8346c2f149f22efd224be3
Architect 3 <architect_3@tomato.com>
2019-12-11 21:04:40 +0530

    Addition is overhead on 'onion' processor.Check for zero before addition !

--- add.c
+++ add.c
@@ -6,6 +6,12 @@ int32_t add(int32_t a,int32_t b)
    int32_t x=0,y=0,z=0;
    x = a;
    y = b;
+
+   if (y == 0)
+       return x;
+   if (x == 0)
+       return y;
+
    z = x + y;
    return (z);
 }
