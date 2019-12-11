c639b62e61b26d868b928120a03735f2fad7226d
Intern 1 <intern_1@tomato.com>
2019-12-11 18:40:52 +0530

    User story-4 done.Added more statements

--- main.c
+++ main.c
@@ -7,5 +7,10 @@ int main()
 {
   printf("%d + %d = %d\n",1,2,add(1,2));
   printf("%d + %d = %d\n",2,4,add(2,4));
+  printf("%d + %d = %d\n",10,20,add(10,20));
+  printf("%d + %d = %d\n",100,30,add(100,30));
+  printf("%d + %d = %d\n",40,53,add(40,53));
+  printf("%d + %d = %d\n",37,43,add(37,43));
+  printf("%d + %d = %d\n",12,23,add(12,23));
   return 0;
 }
