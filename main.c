bd28214daaf5a95636742ff3f72030338f4bebb8
Developer 1 <developer_1@tomato.com>
2019-12-11 19:07:08 +0530

    User story-5 done.Minor refactoring

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "add.h"
 
 int32_t add(int32_t a,int32_t b);
 
-int main()
+int main(int argc,char *argv[])
 {
   printf("%d + %d = %d\n",1,2,add(1,2));
   printf("%d + %d = %d\n",2,4,add(2,4));
@@ -12,5 +13,5 @@ int main()
   printf("%d + %d = %d\n",40,53,add(40,53));
   printf("%d + %d = %d\n",37,43,add(37,43));
   printf("%d + %d = %d\n",12,23,add(12,23));
-  return 0;
+  return EXIT_SUCCESS;
 }
